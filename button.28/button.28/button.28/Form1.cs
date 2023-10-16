1a6a1356952aa4cb4f0afb2ede01e018001fb77d
11meryem <145452149+11meryem@users.noreply.github.com>
2023-10-16 14:32:02 +0300

    Add files via upload

---
+++ button.28/button.28/button.28/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button._28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int gunno = comboBox1.SelectedIndex;
+            gunno++;
+            if (gunno == 1 || gunno == 2 || gunno == 3 || gunno == 4 || gunno == 5)
+            {
+                label2.Text = "çalışma saatleri 9.00'dan 15.00a kadar";
+
+            }
+
+            else if(gunno == 6)
+            {
+                label2.Text = "çalışma saatleri 9dan 13e kadar";
+            }
+
+            else
+            {
+                label2.Text = "tatil";
+            }
+         
+        }
+    }
+}
